7fc0a7f3210dfec4d28ecf04cead66befb43aeb9
Bruce Hsu <brucehsu@fontrip.com>
2017-11-13 11:56:48 +0800

    add chai.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,7 @@
 var assert = require('assert');
+var expect = require('chai').expect;
+
+
 
 describe('Array', () =>{
 	describe('#indexOf()', ()=>{
@@ -9,6 +12,15 @@ describe('Array', () =>{
 	describe('#length()', ()=>{
 		it('should return 3 when has three element', () => {
 			assert.equal(3, [1,2,3].length);
+		});
+	});
+});
+
+var foo = 'bar';
+describe('Foo', () =>{
+	describe('#expect', ()=>{
+		if('expect a string', ()=>{
+			expect(foo).to.be.a('string');
 		});
 	});
 });
